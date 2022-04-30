e06d08bbd2f6ac03b0d2df5d2360028d81a32632
jacktembo <jack@jacktembo>
2022-05-01 01:10:59 +0200

    setup staticfiles

--- eventstickets/settings.py
+++ eventstickets/settings.py
@@ -145,7 +145,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+# STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 
 # Default primary key field type
